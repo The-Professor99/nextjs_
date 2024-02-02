9147c94a4e09e91fae9abd2f76e334b1822a0fe3
The-Professor99 <festusihechi99@gmail.com>
2024-02-02 16:32:24 +0100

    Add all completed files

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+ 
+// initialize NextAuth.js with the authConfig object and 
+// exporting the auth property
+export default NextAuth(authConfig).auth;
+ 
+export const config = {
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
